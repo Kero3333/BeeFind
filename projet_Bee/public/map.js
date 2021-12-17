b48fbfd00b9a522e59ff07a0496d93c9e314ba32
Kévin Robic <kevin.r@institutsolacroup.com>
2021-12-17 09:34:28 +0100

    mise à jour la map

--- projet_Bee/public/map.js
+++ projet_Bee/public/map.js
@@ -5,18 +5,32 @@ function initMap() {
     })
     .then((data) => {
       data.forEach((picture) => {
-        const latt = parseFloat(picture.localisation.split("/")[0]);
-        const long = parseFloat(picture.localisation.split("/")[1]);
+        if (picture.localisation !== "undefined/undefined") {
+          const latt = parseFloat(picture.localisation.split("/")[0]);
+          const long = parseFloat(picture.localisation.split("/")[1]);
 
-        let test = { lat: latt, lng: long };
-        const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: test,
-        });
-        const mark = new google.maps.Marker({
-          position: test,
-          map: map,
-        });
+          let loca = { lat: latt, lng: long };
+          const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: loca,
+          });
+          const mark = new google.maps.Marker({
+            position: loca,
+            map: map,
+          });
+        } else {
+          const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 8,
+            center: { lat: 47.869906, lng: -3.303309 },
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: { lat: 47.869906, lng: -3.303309 },
       });
     });
 }
